322d975fdf7164e7c34c61a250fa0549e0522640
Krzysztof Krupa <krzysztof.krupa@giax.pl>
2021-07-13 12:55:01 +0200

    Add project files.

---
+++ SnakeGame/Helpers/RegistryHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System.Linq;
+using System.Reflection;
+
+namespace SnakeGame.Helpers
+{
+    public static class RegistryHelper
+    {
+        public static string AssemblyName { get; } = Assembly.GetExecutingAssembly().GetName().Name;
+
+        public static void WriteRegistry(string nodeName, string value)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software", true);
+            key = !key.GetSubKeyNames().Contains(AssemblyName) ? key.CreateSubKey(AssemblyName, true) : key.OpenSubKey(AssemblyName, true);
+
+            key.SetValue(nodeName, value, RegistryValueKind.String);
+        }
+
+        public static string GetRegistry(string nodeName)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software", true).CreateSubKey(AssemblyName, true);
+
+            return key != null ? key.GetValue(nodeName, string.Empty).ToString() : string.Empty;
+        }
+    }
+}
